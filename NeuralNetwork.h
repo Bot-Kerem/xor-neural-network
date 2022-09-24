02b3314517764010d7feb00fd9c6d7536574d8a0
Bot-Kerem <akerem1434@gmail.com>
2022-09-24 19:07:42 +0300

    Final shit

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -2,10 +2,16 @@
 #define NEURAL_NETWORK_H
 
 #include "Neuron.h"
+#include "Dataset.h"
 
-inline double activation(double value)
+inline double Sigmoid(double value)
 {
     return value / (1 + abs(value));
+}
+
+inline double deltaSigmoid(double value)
+{
+    return value * (1 - value);
 }
 
 template <size_t numInputs, size_t numHiddenNeurons, size_t numOutputs>
@@ -14,6 +20,103 @@ class NeuralNetwork
     public:
         std::array<Neuron<numInputs>, numHiddenNeurons> HiddenLayer;
         std::array<Neuron<numHiddenNeurons>, numOutputs> OutputLayer;
+
+        void Train(auto&& dataset, int Epochs, double LearningRate)
+        {
+            for(int epoch = 0; epoch < Epochs; epoch++)
+            {
+                dataset.Shuffle();
+
+                for(auto&& data: dataset.Data)
+                {
+                    // Compute hidden layer activation
+                    for(auto& hiddenNeuron: HiddenLayer)
+                    {
+                        double activation = hiddenNeuron.Bias;
+                        for(size_t i = 0; i < numInputs; i++)
+                        {
+                            activation += data.Inputs[i] * hiddenNeuron.Weights[i];
+                        }
+                        hiddenNeuron.Value = Sigmoid(activation);
+                    }
+
+                    // Compute output layer activation
+                    for(auto& outputNeuron: OutputLayer)
+                    {
+                        double activation = outputNeuron.Bias;
+                        
+                        for(size_t i = 0; i < numHiddenNeurons; i++)
+                        {
+                            activation += outputNeuron.Value * outputNeuron.Weights[i];
+                        }
+                        outputNeuron.Value = Sigmoid(activation); 
+                    }
+
+                    printf("Input:");
+                    for(auto& input: data.Inputs)
+                    {
+                        printf(" %g", input);
+                    }
+
+                    printf("\tOutput:");
+                    for(auto& output: data.Outputs)
+                    {
+                        printf(" %g", output);
+                    }
+
+                    printf("\tPredict:");
+                    for(auto& output: OutputLayer)
+                    {
+                        printf(" %g", output.Value);
+                    }
+                    printf("\n");
+
+                    // Backprop
+
+                    // Compute change in output weights
+                    double deltaOutput[numOutputs];
+
+                    for(int j = 0; j < numOutputs; j++)
+                    {
+                        double error = data.Outputs[j] - OutputLayer[j].Value;
+                        deltaOutput[j] = error * deltaSigmoid(OutputLayer[j].Value);
+                    }
+
+                    // Compute change in hidden weights
+                    double deltaHidden[numHiddenNeurons];
+                    for(int j = 0; j < numHiddenNeurons; j++)
+                    {
+                        double error = 0.0f;
+                        for(int k = 0; k < numOutputs; k++)
+                        {
+                            error += deltaOutput[k] * OutputLayer[k].Weights[j];
+                        }
+                        deltaHidden[j] = error * deltaSigmoid(HiddenLayer[j].Value);
+                    }
+
+                    // Apply change in output weights
+                    for(int j = 0 ; j < numOutputs; j++)
+                    {
+                        OutputLayer[j].Bias += deltaOutput[j] * LearningRate;
+                        for(int k = 0; k < numHiddenNeurons; k++)
+                        {
+                            OutputLayer[j].Weights[k] += HiddenLayer[k].Value * deltaOutput[j] * LearningRate;
+                        }
+                    }
+
+                    // Apply change in hidden weights
+                    for(int j = 0 ; j < numHiddenNeurons; j++)
+                    {
+                        HiddenLayer[j].Bias += deltaHidden[j] * LearningRate;
+                        for(int k = 0; k < numInputs; k++)
+                        {
+                            HiddenLayer[j].Weights[k] += data.Inputs[k] * deltaHidden[j] * LearningRate;
+                        }
+                    }
+                    // TODO: PRINT FINAL BIASES / WEIGHTS
+                }
+            }
+        }
 };
 
 #endif // NEURAL_NETWORK_H
--- main.cpp
+++ main.cpp
@@ -10,29 +10,8 @@ int main()
     srand(time(NULL));
     NeuralNetwork<2, 3, 1> network;
     
+    Dataset<double, 4, 2, 1> dataset({1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0});
+    network.Train(dataset, 10000, 0.1);
 
-    Dataset<double, 4, 2, 1> a({1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0});
-    for (auto &&data : a.Data)
-    {
-        for (auto &&input : data.Inputs)
-            printf("Input: %lf\n", input);
-        for (auto &&output : data.Outputs)
-            printf("Output: %lf\n", output);
-    }
-
-    a.Shuffle();
-    printf("Shuffle\n");
-    for (auto &&data : a.Data)
-    {
-        for (auto &&input : data.Inputs)
-            printf("Input: %lf\n", input);
-        for (auto &&output : data.Outputs)
-            printf("Output: %lf\n", output);
-    }
-
-    for (auto &&i : network.HiddenLayer)
-    {
-        printf("Bias: %lf\n", i.Bias);
-    }
     return 0;
 }
